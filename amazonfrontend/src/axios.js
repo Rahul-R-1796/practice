7ad9e3dabc6ddc674165b85b919ce85fda9e9fc5
Rahul R <127472805+Rahul-R-1796@users.noreply.github.com>
2023-09-13 19:45:59 +0530

    Update axios.js

--- amazonfrontend/src/axios.js
+++ amazonfrontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://amazonbckend.herokuapp.com/",
+  baseURL: "https://amazon-api-p7xy.onrender.com/",
 });
 
 export default instance;
